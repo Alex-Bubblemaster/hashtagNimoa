6f0be96f9b5c9a14b30fe138273d0fcb40dcd557
Alex-Bubblemaster <shiatzu@gmail.com>
2015-12-05 22:59:48 +0200

    Added SadPlayer with only PreFlopLogic

---
+++ SadPlayer/Helpers/CardEvaluator.cs
@@ -0,0 +1,95 @@
+﻿namespace SadPlayer.Helpers
+{
+    using System;
+    using TexasHoldem.Logic.Cards;
+
+    public class CardEvaluator
+    {
+        public static double CalculateStrength(Card one, Card two)
+        {
+            double pp = 0;
+
+            switch (one.Type)
+            {
+                case CardType.Jack:
+                    pp = 6;
+                    break;
+                case CardType.Queen:
+                    pp = 7;
+                    break;
+                case CardType.King:
+                    pp = 8;
+                    break;
+                case CardType.Ace:
+                    pp = 10;
+                    break;
+                default:
+                    pp = (int)one.Type / (double)2;
+                    break;
+            }
+
+            double pp2 = 0;
+            switch (two.Type)
+            {
+                case CardType.Jack:
+                    pp2 = 6;
+                    break;
+                case CardType.Queen:
+                    pp2 = 7;
+                    break;
+                case CardType.King:
+                    pp2 = 8;
+                    break;
+                case CardType.Ace:
+                    pp2 = 10;
+                    break;
+                default:
+                    pp2 = (int)one.Type / (double)2;
+                    break;
+            }
+
+            var points = Math.Max(pp, pp2);
+
+            if (one.Type == two.Type)
+            {
+                points *= 2;
+                if (points < 5)
+                {
+                    points = 5;
+                }
+            }
+
+            if (one.Suit == two.Suit)
+            {
+                points += 2;
+            }
+
+            if (Math.Abs(one.Type - two.Type) == 1)
+            {
+                points++;
+            }
+
+            if (Math.Abs(one.Type - two.Type) == 2)
+            {
+                points--;
+            }
+
+            if (Math.Abs(one.Type - two.Type) == 3)
+            {
+                points -= 2;
+            }
+
+            if (Math.Abs(one.Type - two.Type) == 4)
+            {
+                points -= 4;
+            }
+
+            if (Math.Abs(one.Type - two.Type) >= 5)
+            {
+                points -= 5;
+            }
+
+            return points;
+        }
+    }
+}
---
+++ SadPlayer/NimoaSadPlayer.cs
@@ -0,0 +1,61 @@
+﻿namespace SadPlayer
+{
+    using System;
+    using System.Collections.Generic;
+    using Helpers;
+    using TexasHoldem.Logic;
+    using TexasHoldem.Logic.Cards;
+    using TexasHoldem.Logic.Helpers;
+    using TexasHoldem.Logic.Players;
+
+    public class NimoaSadPlayer : BasePlayer // Bill Chen formula http://www.simplyholdem.com/chen.html
+    {
+        private static double ownCardsStrength;
+        private static HandEvaluator evaluator = new HandEvaluator();
+
+        public override string Name { get; } = "Adrian.Bozhankov_" + Guid.NewGuid();
+
+        public override PlayerAction GetTurn(GetTurnContext context)
+        {
+            if (context.RoundType == GameRoundType.PreFlop)
+            {
+                ownCardsStrength = CardEvaluator.CalculateStrength(this.FirstCard, this.SecondCard);
+                if (ownCardsStrength >= 9)
+                {
+                    return PlayerAction.Raise((int)ownCardsStrength * context.SmallBlind);
+                }
+
+                if (ownCardsStrength >= 8)
+                {
+                   return PlayerAction.CheckOrCall();
+                }
+
+                return PlayerAction.Fold();
+            }
+
+            if (context.RoundType == GameRoundType.Flop)
+            {
+                var cardsToevaluate = new List<Card>();
+
+                foreach (var cc in this.CommunityCards)
+                {
+                    cardsToevaluate.Add(cc);
+                }
+
+                cardsToevaluate.Add(this.FirstCard);
+                cardsToevaluate.Add(this.SecondCard);
+                var bestCard = evaluator.GetBestHand(cardsToevaluate);
+                if (bestCard.RankType == 0)
+                {
+                    return PlayerAction.Fold();
+                }
+                else
+                {
+                    return PlayerAction.CheckOrCall();
+                }
+            }
+
+            return PlayerAction.CheckOrCall();
+        }
+    }
+}
