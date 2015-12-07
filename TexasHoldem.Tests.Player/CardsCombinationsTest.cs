c11c63108295421f4a3780639797eb44ab84f050
damy90 <dany90@gmail.com>
2015-12-07 13:57:07 +0200

    Added unit test for random cards set generator

---
+++ TexasHoldem.Tests.Player/CardsCombinationsTest.cs
@@ -0,0 +1,26 @@
+﻿namespace TexasHoldem.Tests.Player
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+    using AI.NimoaPlayer.Helpers;
+    using System.Linq;
+
+    [TestFixture]
+    public class CardsCombinationsTest
+    {
+        [Test]
+        public void GenerateRandomCardsShouldGenerateUniqueCombinations()
+        {
+            var randomCombinations = CardsCombinations.GetRandomCardsIndexes(20, 2, 5);
+            var uniqueCombinations=new HashSet<string>(randomCombinations.Select(x => string.Join(", ", x)));
+
+            Assert.AreEqual(randomCombinations.Count, uniqueCombinations.Count);
+
+            foreach (var randomCombination in randomCombinations)
+            {
+                Assert.AreNotEqual(randomCombination[0], randomCombination[1]);
+            }
+        }
+    }
+}
